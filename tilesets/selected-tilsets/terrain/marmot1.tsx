2aaaf438c547b2b8f2df59074ccc3e1fe1f4be75
ClydeWallace22 <48610606+BrandtH22@users.noreply.github.com>
2024-03-19 08:54:18 -0600

    include decorations and misc assets

---
+++ tilesets/selected-tilsets/terrain/marmot1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="marmot1" tilewidth="32" tileheight="32" tilecount="2304" columns="48">
+ <image source="../../../src/assets/terrain/marmot1.jpg" width="1536" height="1536"/>
+</tileset>
